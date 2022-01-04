161c7378a455c12f76c49d28f01eee293797eecd
Richard Olaniyan <71474562+RichardOlaniyan@users.noreply.github.com>
2022-01-04 16:13:28 -0500

    Deeper dive into Py and React

---
+++ LearningPython/Learningpythw/ex1.py
@@ -0,0 +1,7 @@
+#print('I love berries')
+print('Hello World')
+print('Hello Again')
+print('I like typing this')
+print('This is fun')
+print('Yay, printing')
+print("I'll rather not have you 'not'.")
---
+++ LearningPython/Learningpythw/ex3.py
@@ -0,0 +1,28 @@
+#This line prints out the words in ''
+print('I will now count my chickens:')
+#This line prints out Hens and also the result of the calculation
+print('Hens', 25 + 30 / 6)
+#This line prints out Roosters and also the result of the calculation
+print('Roosters', 100 - 25 * 3 % 4)
+#This line prints out the words in ''
+print('Now I will count the eggs:')
+#This line prints out the result of the expression
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+#This line prints out the words in ''
+print('Is it true that 3 + 2 < 5 - 7?')
+#This line prints out the result of the expression
+print(3 + 2 < 5 - 7)
+#This line prints out "" and also the result of the calculation
+print('What is 3 + 2?', 3 + 2)
+#This line prints out Hens and also the result of the calculation
+print("What is 5 - 7?", 5 - 7)
+#This line prints out the words in ''
+print("Oh, that's why it's False.")
+#This line prints out the words in ''
+print("How about some more")
+#This line prints out Hens and also the result of the calculation
+print("Is it greater?", 5 > 2)
+#This line prints out Hens and also the result of the calculation
+print("Is it greater or equal?", 5 >= -2)
+#This line prints out Hens and also the result of the calculation
+print("Is it less or equal?", 5 <= -2)
